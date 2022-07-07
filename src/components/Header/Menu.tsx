8eb20cdf5930d276c269160c9c561c2e82eb2111
kavispires <kavispires@gmail.com>
2022-07-06 19:16:34 -0700

    Fixed Menu

--- src/components/Header/Menu.tsx
+++ src/components/Header/Menu.tsx
@@ -1,42 +1,31 @@
-import { Menu as AntMenu } from 'antd';
-import type { MenuProps } from 'antd';
-import { useNavigate, useLocation } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 import { CloudSyncOutlined, HomeOutlined, TableOutlined } from '@ant-design/icons';
+import { Link } from 'react-router-dom';
+import clsx from 'clsx';
 
 export function Menu() {
-  const navigate = useNavigate();
   const location = useLocation();
-
-  const items: MenuProps['items'] = [
-    {
-      label: 'Home',
-      key: 'home',
-      icon: <HomeOutlined />,
-    },
-    {
-      label: 'Shelves',
-      key: 'shelves',
-      icon: <TableOutlined />,
-    },
-    {
-      label: 'Sync',
-      key: 'sync',
-      icon: <CloudSyncOutlined />,
-    },
-  ];
-
-  const onClick: MenuProps['onClick'] = (e) => {
-    navigate(e.key);
-  };
+  const currentRoute = location.pathname;
 
   return (
-    <AntMenu
-      onClick={onClick}
-      mode="horizontal"
-      selectedKeys={[location.pathname.substring(1)]}
-      items={items}
-      theme="dark"
-      className="header-menu"
-    />
+    <nav className="header-menu">
+      <Link className={clsx('header-menu-link', currentRoute === '/' && 'header-menu-link--active')} to={'/'}>
+        <HomeOutlined /> Home
+      </Link>
+
+      <Link
+        className={clsx('header-menu-link', currentRoute === '/shelves' && 'header-menu-link--active')}
+        to={'/shelves'}
+      >
+        <TableOutlined /> Shelves
+      </Link>
+
+      <Link
+        className={clsx('header-menu-link', currentRoute === '/sync' && 'header-menu-link--active')}
+        to={'/sync'}
+      >
+        <CloudSyncOutlined /> Sync
+      </Link>
+    </nav>
   );
 }
